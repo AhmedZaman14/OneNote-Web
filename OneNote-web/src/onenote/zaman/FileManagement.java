37715efeae70a27acd0068c1da91479a3f8c98e2
AhmedZaman14 <zaman994ahmed@gmail.com>
2022-12-20 23:55:01 +0500

    Project phase 3- web services project file added

---
+++ OneNote-web/src/onenote/zaman/FileManagement.java
@@ -0,0 +1,214 @@
+package onenote.zaman;
+import java.io.BufferedWriter;
+import java.io.File;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+
+
+
+
+public class FileManagement {
+
+
+
+    private int getNotebookID(int user_id) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/onenote", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select NotebookID from NoteBook where user_id = '" + user_id + "'");
+            rs.next();
+            return rs.getInt(1);
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return -1;
+    }
+
+
+    private int getSectionID(int notebookID) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/onenote", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select SectionID from Section where NoteBookId = '" + notebookID + "' and SectionName = 'Education/Work'");
+            rs.next();
+            return rs.getInt(1);
+
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return -1;
+    }
+
+    private int getPageId(int sectionID, String pageTitle) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/onenote", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select page_id from Page where  pageTitle= '" + pageTitle + "'  AND (SectionId = '" + sectionID + "' or SectionId = '" + (sectionID + 1) + "' or SectionId = '" + (sectionID + 2) + "')");
+            if (rs.next()) {
+                return rs.getInt(1);
+
+            } else {
+
+                return -1;
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return -1;
+    }
+
+    private int getNoteID(int pageID) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/onenote", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select NoteID from Note where page_id = '" + pageID + "' ");
+            if (rs.next()) {
+                return rs.getInt(1);
+
+            } else {
+
+                return -1;
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return -1;
+    }
+
+    private void deltePage(int pageID, String PageTitle) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/onenote", "root", "");
+            Statement stmt = con.createStatement();
+
+            stmt.executeUpdate("delete from Page where page_id = '" + pageID + "' and PageTitle= '" + PageTitle + "' ");
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    private void deleteNoteCategory(int noteID) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/onenote", "root", "");
+            Statement stmt = con.createStatement();
+
+            stmt.executeUpdate("delete from Category where note_id = '" + noteID + "'");
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    private void deleteNoteLayout(int noteID) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/onenote", "root", "");
+            Statement stmt = con.createStatement();
+
+            stmt.executeUpdate("delete from NoteLayout where note_id = '" + noteID + "'");
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+
+    public String deleteNote(int user_id, String noteName) {
+
+        int notebookID = getNotebookID(user_id);
+        int sectionID = getSectionID(notebookID);
+        int pageID = getPageId(sectionID, noteName);
+        int noteID = getNoteID(pageID);
+        if (notebookID != -1 && sectionID != -1 && pageID != -1 && noteID != -1) {
+
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/onenote", "root", "");
+                Statement stmt = con.createStatement();
+                deleteNoteCategory(noteID);
+                deleteNoteLayout(noteID);
+                stmt.executeUpdate("delete from Note where page_id = '" + pageID + "'");
+                deltePage(pageID, noteName);
+
+
+                return "Note Deleted";
+
+            } catch (Exception e) {
+                e.getMessage();
+
+            }
+
+            return "Error Note couldn't deleted";
+
+        } else {
+            return "Note Does not exists";
+        }
+    }
+
+
+//========================================Add File To Your PC====================================
+
+    //to save the note to ur PC
+
+
+    public String addFile(int user_id, String noteName, String filepath) {
+    	  try {
+    	    Class.forName("com.mysql.jdbc.Driver");
+    	    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote", "root", "");
+    	    Statement stmt = con.createStatement();
+    	    ResultSet rs = stmt.executeQuery("select PageTitle, Text from page\r\n" +
+    	      "JOIN section ON section.SectionID = page.SectionID\r\n" +
+    	      "JOIN notebook ON notebook.NoteBookID = section.NoteBookID\r\n" +
+    	      "JOIN user ON user.user_id = notebook.user_id\r\n" +
+    	      "JOIN note ON note.page_id = page.page_id\r\n" +
+    	      "where user.user_id = '" + user_id + "' AND page.PageTitle = '" + noteName + "'\r\n" +
+    	      "");
+    	    rs.next();
+    	    String noteTitle = rs.getString(1);
+    	    String txt = rs.getString(2);
+
+    	    // Check if the filepath is valid
+    	    File file = new File(filepath);
+    	    if (!file.exists() || !file.isDirectory()) {
+    	      throw new IOException("The filepath is invalid or does not exist. Please enter a valid filepath.");
+    	    }
+
+    	    // Call the saveFile method inside the try-catch block
+    	    saveFile(filepath, noteTitle, txt);
+    	    con.close();
+    	    return "File created successfully at: " + filepath + noteTitle + ".txt";
+    	  } catch (IOException e) {
+    	    System.out.println(e.getMessage());
+    	    return "Error: " + e.getMessage();
+    	  } catch (Exception e) {
+    	    System.out.println(e);
+    	    return "Error: " + e.getMessage();
+    	  }
+    	}
+
+    private void saveFile(String filepath, String noteTitle, String txt) throws IOException {
+    	  try {
+    	    // Add a trailing slash to the filepath if it doesn't have one
+    	    if (!filepath.endsWith("/")) {
+    	      filepath += "/";
+    	    }
+    	    FileWriter fileWriter = new FileWriter(filepath + noteTitle + ".txt");
+    	    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+    	    bufferedWriter.write(txt);
+    	    bufferedWriter.close();
+    	  } catch (Exception e) {
+    	    e.getMessage();
+    	  }
+    	}
+
+
+}
---
+++ OneNote-web/src/onenote/zaman/Notes.java
@@ -0,0 +1,248 @@
+package onenote.zaman;
+
+import java.sql.*;
+import java.util.Date;
+
+public class Notes {
+	private int getNoteBookid(int user_id) {
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote","root","");
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("select NotebookID from notebook where user_id='"+user_id+"'");
+		rs.next();
+		return rs.getInt(1);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		System.out.println("NoteBook does not exists for this user:");
+		return -1;
+	}
+	
+	private int getSectionid(int user_id ,String sectionName) {
+		int noteBookid  =getNoteBookid(user_id);
+		if(noteBookid!=-1) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote","root","");
+				Statement stmt=con.createStatement();
+				ResultSet rs=stmt.executeQuery("select SectionID from Section where notebookid='"+noteBookid+"' and "
+						+ "SectionName ='"+sectionName+"'");
+				rs.next();
+				return rs.getInt(1);
+				}
+				catch(Exception e) {
+					System.out.println(e);
+				}
+		}
+		System.out.println("error in getsection");
+		return -1;
+	}
+	
+	private boolean checkPageExistance(String pageTitle, int user_id) {
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote","root","");
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("select PageTitle from Page\r\n" + 
+				"JOIN section ON section.SectionID = page.SectionID\r\n" + 
+				"JOIN notebook ON notebook.NoteBookID = section.NoteBookID\r\n" + 
+				"JOIN user ON user.user_id = notebook.user_id\r\n" + 
+				"JOIN note ON note.page_id = page.page_id\r\n" + 
+				"where user.user_id = '"+user_id+"' AND page.PageTitle = '"+pageTitle+"'\r\n" + 
+				"");
+		if(rs.next())
+		return true;
+		
+		} catch(Exception e)
+		{
+		System.out.println(e);
+		}
+
+		
+		return false;
+	}
+
+	
+	private int createPage(String pageTitle, int user_id, String sectionName) {
+		int sectionId = getSectionid(user_id,sectionName);
+		if(sectionId !=-1 && !checkPageExistance(pageTitle, user_id)) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote","root","");
+				
+				
+				Statement stmt=con.createStatement();
+				stmt.executeUpdate("insert into Page(pageTitle,SectionId) values('"+pageTitle+"', '"+sectionId+"')", Statement.RETURN_GENERATED_KEYS);
+				 ResultSet generatedKeys = stmt.getGeneratedKeys();
+				    if (generatedKeys.next()) {
+				      return generatedKeys.getInt(1);
+				    }
+				
+				}
+				catch(Exception e) {
+					System.out.println(e);
+				}
+		}
+		//System.out.println("error in createPage");
+		return -1;
+		
+	}
+	
+
+	
+	
+	
+	
+	private void insertDefaultLayout(int noteID){
+		if(noteID !=-1) {
+			  try {
+			    Class.forName("com.mysql.jdbc.Driver");
+			    Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote","root","");
+			    Statement stmt = con.createStatement();
+			    stmt.executeUpdate("INSERT INTO NoteLayout(note_id,textColor,fontName) VALUES( '"+noteID+"', 'Black', 'Georgia')");
+			    
+			   
+			  } catch (Exception e) {
+			    System.out.println(e);
+			  }
+			}
+	}
+	
+	private void insertIntoNoteCategory(String txt,int noteID) {
+		String category = getNoteCategory(txt);
+		if(noteID !=-1) {
+			  try {
+			    Class.forName("com.mysql.jdbc.Driver");
+			    Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote","root","");
+			    Statement stmt = con.createStatement();
+			    stmt.executeUpdate("INSERT INTO category(CategoryName,note_id) VALUES('"+category+"', '"+noteID+"')");
+			    
+			   
+			  } catch (Exception e) {
+			    System.out.println(e);
+			  }
+			}
+			
+	}
+	
+	
+	private String getNoteCategory( String txt) {
+		  
+		  String[] schoolKeywords = {"assignment", "quiz", "home work", "class"};
+		  String[] workKeywords = {"presentation", "job", "boss", "deadline", "task"};
+		  String[] domiciliaryKeywords = {"car", "home", "groceries", "bill", "family"};
+
+		  for (String keyword : schoolKeywords) {
+		    if (txt.matches(".*\\b" + keyword + "\\b.*")) {
+		      return "School";
+		    }
+		  }
+		  for (String keyword : workKeywords) {
+		    if (txt.matches(".*\\b" + keyword + "\\b.*")) {
+		      return "Work";
+		    }
+		  }
+		  for (String keyword : domiciliaryKeywords) {
+		    if (txt.matches(".*\\b" + keyword + "\\b.*")) {
+		      return "Domiciliary";
+		    }
+		  }
+
+		  return "Other";
+		}
+		
+
+
+
+
+
+	
+	
+	
+	private int noteID(String txt,int pageID) {
+		  try {
+			  Date currentDate = new Date();
+			  Timestamp currentTimestamp = new Timestamp(currentDate.getTime());
+		    Class.forName("com.mysql.jdbc.Driver");
+		    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote", "root", "");
+		    Statement stmt = con.createStatement();
+		    stmt.executeUpdate("INSERT INTO Note(Text,CreationDate, page_id) VALUES('" + txt + "', '" + currentTimestamp + "', '" + pageID + "')", Statement.RETURN_GENERATED_KEYS);
+
+		    // Get the generated ID
+		    ResultSet generatedKeys = stmt.getGeneratedKeys();
+		    if (generatedKeys.next()) {
+		      return generatedKeys.getInt(1);
+		    }
+		  } catch (Exception e) {
+		    System.out.println(e);
+		  }
+		  return -1;
+		}
+	
+	
+	
+	public String createANote(String pageTitle, String txt,String sectionName, int user_id) {
+		  
+		int noteID= -1;
+		//this method returns the last inserted id
+		if(checkPageExistance(pageTitle, user_id)) {
+			return "Plz Change PageTitle because Page with this name already exists:";
+		}
+		
+		int pageID = createPage(pageTitle,user_id,sectionName);
+		if(pageID!=-1 ) {
+			try {
+				noteID = noteID(txt,pageID);
+				insertIntoNoteCategory(txt,noteID);
+				insertDefaultLayout(noteID);
+				
+				return "Note Created";
+				}
+				catch(Exception e) {
+					System.out.println(e);
+				}
+		}
+		
+		return "Plz provide correct information to create a Note";
+		
+	    
+	}
+	
+	
+
+	public String searchNote(int user_id, String noteName) {
+		try{
+			/*
+			this userId is important in parameter
+			because user will search his own Notes which he had created-
+			latter user_id can be removed from the parameters. 
+			*/
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3307/OneNote","root","");
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("select PageTitle, Text, CreationDate from Page\r\n" + 
+					"JOIN section ON section.SectionID = page.SectionID\r\n" + 
+					"JOIN notebook ON notebook.NoteBookID = section.NoteBookID\r\n" + 
+					"JOIN user ON user.user_id = notebook.user_id\r\n" + 
+					"JOIN note ON note.page_id = page.page_id\r\n" + 
+					"where user.user_id = '"+user_id+"' AND page.PageTitle = '"+noteName+"'\r\n" + 
+					"");
+			rs.next();
+			String displayNote = "Note Title: " + rs.getString(1) + "\tNote: " + rs.getString(2) +"\tCreationData: "+ rs.getString(3);
+			con.close();
+			return displayNote;
+			
+			} catch(Exception e)
+			{
+			System.out.println(e);
+			}
+		return "No Such Note Found!";
+		
+		}
+	
+	
+	
+}
